533c11f449ca556f6250eefd5f72fd9e3a569248
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-06-27 01:44:40 +0100

    task_1-4

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,3 +1,11 @@
-function convertCurrency({ amount, currency }) {
+type Currency = "USD" | "EUR" | "GBP";
+
+interface ConvertCurrencyParams {
+  amount: number;
+  currency: Currency;
+}
+function convertCurrency({ amount, currency }: ConvertCurrencyParams): void {
   console.log(`Converting ${amount} to ${currency}`);
 }
+
+convertCurrency({ amount: 200, currency: "USD" });
--- src/task-4.ts
+++ src/task-4.ts
@@ -1,1 +1,4 @@
-const dimensions = [1920, 1080];
+let dimensions: number[] = [1920, 1080];
+console.log(
+  `The dimensions of the screen are ${dimensions[0]}x${dimensions[1]}`
+);
